e1652e2611d05140c11b0d3e480d52cdb8b0f637
Sanderson Rocha <sanduba753@gmail.com>
2026-04-20 09:31:05 -0300

    Realocation of calc rules to the service

--- app/domain/services/premium_calculator.py
+++ app/domain/services/premium_calculator.py
@@ -7,33 +7,40 @@ class PremiumCalculator:
     def __init__(self, config):
         self.config = config
 
-    def calculate_car_age(self, car_year: int) -> int:
+    def __calculate_car_age(self, car_year: int) -> int:
         current_year = datetime.now().year
         return current_year - car_year
 
     def calculate_rate(self, car_year: int, car_value: float) -> float:
-        car_age = self.calculate_car_age(car_year)
+        car_age = self.__calculate_car_age(car_year)
         age_rate = car_age * self.config.rate_per_year
         value_rate = (
             car_value / self.config.value_step
         ) * self.config.rate_per_value_step
         return age_rate + value_rate
 
+    def __premium_calculation(
+        self, car, deductible_percentage: float, broker_fee: float, rate: float
+    ):
+        base_premium = car.value * rate
+        discount = base_premium * deductible_percentage
+        return base_premium - discount + broker_fee
+
     def calculate(
         self, car, deductible_percentage: float, broker_fee: float, rate: float
     ) -> PremiumResult:
-        base_premium = car.value * rate
-        discount = base_premium * deductible_percentage
-        final_premium = base_premium - discount + broker_fee
 
         base_policy_limit = car.value * self.config.coverage_percentage
         deductible_value = base_policy_limit * deductible_percentage
         final_policy_limit = base_policy_limit - deductible_value
+        final_premium = self.__premium_calculation(
+            car, deductible_percentage, broker_fee, rate
+        )
 
         return PremiumResult(
-            car=car,
             applied_rate=rate,
+            car=car,
             calculated_premium=final_premium,
-            policy_limit=final_policy_limit,
             deductible_value=deductible_value,
+            policy_limit=final_policy_limit,
         )
